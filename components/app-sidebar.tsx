6571dbd87d32722d585601f29b89a557c778966e
raamiiChu <ericchu0617@gmail.com>
2025-11-30 23:12:06 +0800

    build: init project

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,73 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { ModeToggle } from "@/components/mode-toggle";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar() {
+  const items = [
+    {
+      title: "Home",
+      url: "/",
+      icon: Home,
+    },
+    {
+      title: "Inbox",
+      url: "/test",
+      icon: Inbox,
+    },
+    {
+      title: "Calendar",
+      url: "#",
+      icon: Calendar,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: Search,
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+  ];
+
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <ModeToggle />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
---
+++ components/typography.tsx
@@ -0,0 +1,237 @@
+import { cn } from "@/lib/utils";
+
+export function H1({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <h1
+      className={cn(
+        "mb-6 scroll-m-20 font-extrabold text-4xl tracking-tight lg:text-5xl",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function H2({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <h2
+      className={cn(
+        "mb-2 scroll-m-20 border-b pb-2 font-semibold text-3xl tracking-tight first:mt-0",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function H3({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <h3
+      className={cn(
+        "mb-2 scroll-m-20 font-semibold text-2xl tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function H4({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <h4
+      className={cn(
+        "mb-2 scroll-m-20 font-semibold text-xl tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function P({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLParagraphElement>) {
+  return <p className={cn("mb-2 leading-7", className)} {...props} />;
+}
+
+export function Blockquote({
+  className,
+  ...props
+}: React.BlockquoteHTMLAttributes<HTMLQuoteElement>) {
+  return (
+    <blockquote
+      className={cn(
+        "mt-6 border-slate-300 border-l-2 pl-6 text-muted-foreground italic",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function Highlight({
+  className,
+  underline = false,
+  ...props
+}: React.HTMLAttributes<HTMLSpanElement> & { underline?: boolean }) {
+  return (
+    <span
+      className={cn(
+        "font-bold text-destructive",
+        underline && "underline",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function Table({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLTableElement>) {
+  return (
+    <div className="my-6 w-full overflow-y-auto">
+      <table className={cn("w-full", className)} {...props} />
+    </div>
+  );
+}
+
+export function THead({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLTableSectionElement>) {
+  return <thead className={cn("[&_tr]:border-b", className)} {...props} />;
+}
+
+export function TBody({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLTableSectionElement>) {
+  return (
+    <tbody className={cn("[&_tr:last-child]:border-0", className)} {...props} />
+  );
+}
+
+export function TR({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLTableRowElement>) {
+  return (
+    <tr
+      className={cn(
+        "border-b transition-colors hover:bg-muted data-[state=selected]:bg-muted",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function TD({
+  className,
+  ...props
+}: React.TdHTMLAttributes<HTMLTableCellElement>) {
+  return (
+    <td
+      className={cn(
+        "p-4 align-middle [&:has([role=checkbox])]:pr-0",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function TH({
+  className,
+  ...props
+}: React.ThHTMLAttributes<HTMLTableCellElement>) {
+  return (
+    <th
+      className={cn(
+        "h-12 px-4 text-left align-middle font-medium text-foreground [&:has([role=checkbox])]:pr-0",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function List({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLUListElement>) {
+  return (
+    <ul
+      className={cn("my-6 ml-6 list-disc [&>li]:mt-2", className)}
+      {...props}
+    />
+  );
+}
+
+export function InlineCode({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <code
+      className={cn(
+        "relative rounded bg-slate-100 px-[0.3rem] py-[0.2rem] font-mono font-semibold text-slate-900 text-sm",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function Lead({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLParagraphElement>) {
+  return <p className={cn("text-slate-700 text-xl", className)} {...props} />;
+}
+
+export function Large({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("font-semibold text-lg text-slate-900", className)}
+      {...props}
+    />
+  );
+}
+
+export function Small({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLParagraphElement>) {
+  return (
+    <p
+      className={cn("font-medium text-sm leading-none", className)}
+      {...props}
+    />
+  );
+}
+
+export function Muted({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLParagraphElement>) {
+  return <p className={cn("text-slate-500 text-sm", className)} {...props} />;
+}
